fdbbe4161693fd167952abb90ed31af6973515a6
Srivisweswara M S <110775073+vicXsri@users.noreply.github.com>
2023-09-05 11:12:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navigation from './customer/Components/Navbar/Navigation';
+import CustomerRoutes from './Routers/CustomerRoutes';
+import AdminRoutes from './Routers/AdminRoutes';
+import NotFound from './Pages/Notfound';
+import AdminPannel from './Admin/AdminPannel';
+
+function App() {
+  const isAdmin=true;
+  return (
+    <div className="">
+      
+      <Routes>
+        <Route path="/*" element={<CustomerRoutes />} />
+        <Route path="/admin/*" element={<AdminPannel />} />
+        
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
